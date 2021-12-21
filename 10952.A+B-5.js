f2fddfd2390711823db4327c708f4149c77fbf99
bbbjihan <parkjihan5253@gmail.com>
2021-12-22 07:09:04 +0900

    1222

--- 10952.A+B-5.js
+++ 10952.A+B-5.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+const input = fs.readFileSync('input.txt').toString().trim().split('\n');
 
 for(let i = 0; i<input.length-1; i++){
   console.log(Number(input[i].split(' ')[0]) + Number(input[i].split(' ')[1]));
---
+++ 11022.A+B-8.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const input = fs.readFileSync('input.txt').toString().trim().split('\n');
+
+const casecnt = Number(input.shift());
+
+for(let i = 0; i<casecnt; i++){
+    a = Number(input[i].split(' ')[0]);
+    b = Number(input[i].split(' ')[1]);
+    console.log('Case #'+ (i+1).toString() +': '+a.toString()+' + '+b.toString() + ' = '+(a+b).toString());
+}
