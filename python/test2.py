98eddc8e9b01f6a4ad8a66db962eb0aa8f6736ac
JIHAN <143668298+Intellectus-JIHAN@users.noreply.github.com>
2024-04-26 21:38:40 +0900

    20240426 다익스트라

--- python/test2.py
+++ python/test2.py
@@ -1,7 +1,6 @@
-import sys;rl=sys.stdin.readline
-W,H,X,Y,P = map(int,rl().split())
-
-def isInRink(x,y):
-
-
-for _ in range(P):
+graph ={
+  
+}
+graph['A'] = {}
+graph['A']['B'] = 8
+print(graph)
