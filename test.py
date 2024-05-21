e05664522b06640d10003d9c203309bfeaa2313c
JIHAN <74421125+bbbjihan@users.noreply.github.com>
2024-05-21 13:42:19 +0900

    1279

--- test.py
+++ test.py
@@ -4,15 +4,13 @@ N = int(rl())
 
 treeD = {}
 globalMax = 0
-pair = ['','']
+globalPair = [('',200001),('',200001)]
 
 def findPair(candidates, nowDepth, origin):
   if "origin" in nowDepth.keys():
     candidate = nowDepth["origin"]
     if candidate[0] != origin:
       candidates.append(candidate)
-  if len(nowDepth.keys()) == 1:
-    return
   for key in nowDepth.keys():
     if key == 'origin':
       continue
@@ -22,30 +20,42 @@ for i in range(N):
   string = rl().strip()
   nowDepth = treeD
   
-  maxCnt = 0
+  maxCnt = globalMax
   splitDepth = 0
   for j in range(len(string)):
     char = string[j]
     if char in nowDepth.keys():
       maxCnt = max(maxCnt, j + 1)
+      splitDepth = j + 1
     else:
       nowDepth[char] = {}
-      splitDepth = j
     nowDepth = nowDepth[char]
   nowDepth["origin"] = (string,i)
   
-  if globalMax < maxCnt:
-    globalMax = maxCnt
-    pair[1] = string
+  if globalMax <= maxCnt:
     
     nowDepth = treeD
     for j in range(splitDepth):
       nowDepth = nowDepth[string[j]]
     candidates = []
     findPair(candidates, nowDepth, string)
-    if len(candidates) > 0:
-      candidates.sort(key=lambda x: x[1])
-      pair[0] = candidates[0][0]
+    
+    if len(candidates) == 0:
+      continue
+    
+    candidates.sort(key=lambda x: x[1])
+    pair = [candidates[0],(string, i)]
+    
+    if globalMax < maxCnt:
+      globalMax = maxCnt
+      globalPair = pair
+      continue
+    
+    if pair[0][1] < globalPair[0][1]:
+      globalPair = pair
+    elif pair[0][1] == globalPair[0][1]:
+      if globalPair[1][1] > globalPair[1][1]:
+        globalPair = pair
 
-for s in pair:
-  print(s)
+for s in globalPair:
+  print(s[0])
--- test2.py
+++ test2.py
@@ -1,31 +1,49 @@
 import sys;rl=sys.stdin.readline
 
-N,M = map(int,rl().split())
+N = int(rl())
 
-edges = [[] for _ in range(N + 1)]
-indegrees = [0 for _ in range(N + 1)]
-for _ in range(M):
-  start, end = map(int,rl().split())
-  indegrees[end] += 1
-  edges[start].append(end)
+treeD = {}
+globalMax = 0
+pair = ['','']
 
-q = []
+def findPair(candidates, nowDepth, origin):
+  if "origin" in nowDepth.keys():
+    candidate = nowDepth["origin"]
+    if candidate[0] != origin:
+      candidates.append(candidate)
+  for key in nowDepth.keys():
+    if key == 'origin':
+      continue
+    findPair(candidates, nowDepth[key], origin)
 
+for i in range(N):
+  string = rl().strip()
+  nowDepth = treeD
+  
+  maxCnt = globalMax
+  splitDepth = 0
+  for j in range(len(string)):
+    char = string[j]
+    if char in nowDepth.keys():
+      maxCnt = max(maxCnt, j + 1)
+      splitDepth = j + 1
+    else:
+      nowDepth[char] = {}
+    nowDepth = nowDepth[char]
+  nowDepth["origin"] = (string,i)
+  
+  if globalMax < maxCnt:
+    globalMax = maxCnt
+    pair[1] = string
+    
+    nowDepth = treeD
+    for j in range(splitDepth):
+      nowDepth = nowDepth[string[j]]
+    candidates = []
+    findPair(candidates, nowDepth, string)
+    if len(candidates) > 0:
+      candidates.sort(key=lambda x: x[1])
+      pair[0] = candidates[0][0]
 
-for i in range(1, N + 1):
-  if indegrees[i] == 0:
-    q.append(i)
-
-visited = [ False for _ in range(N + 1) ]
-res = []
-while q:
-  now = q.pop(0)
-  visited[now] = True
-  res.append(now)
-  for next in edges[now]:
-    if not visited[next]:
-      indegrees[next] -= 1
-      if indegrees[next] == 0:
-        q.append(next)
-
-print(*res)
+for s in pair:
+  print(s)
