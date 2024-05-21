4a97e48affeacdb84b15baf5f4b507e9fb9609ce
Jihan Park <74421125+bbbjihan@users.noreply.github.com>
2024-05-22 00:53:42 +0900

    20240522ing

--- test.py
+++ test.py
@@ -1,59 +1,56 @@
 import sys;rl=sys.stdin.readline
+import math
 
-N = int(rl())
+N, M, X, Y = map(int,rl().split())
 
-treeD = {}
-globalMax = 0
-globalPair = [('',200001),('',200001)]
+edges = [ [] for _ in range(N + 1) ]
+indegrees = [ 0 for _ in range(N + 1) ]
+minTurn = [ 0 for _ in range(N + 1)]
+successValues = [(-1, -1)]
 
-def findPair(candidates, nowDepth, origin):
-  if "origin" in nowDepth.keys():
-    candidate = nowDepth["origin"]
-    if candidate[0] != origin:
-      candidates.append(candidate)
-  for key in nowDepth.keys():
-    if key == 'origin':
-      continue
-    findPair(candidates, nowDepth[key], origin)
+q = []
+sortedCustomers = []
 
-for i in range(N):
-  string = rl().strip()
-  nowDepth = treeD
-  
-  maxCnt = 0
-  splitDepth = 0
-  for j in range(len(string)):
-    char = string[j]
-    if char in nowDepth.keys():
-      maxCnt = max(maxCnt, j + 1)
-      splitDepth = j + 1
-    else:
-      nowDepth[char] = {}
-    nowDepth = nowDepth[char]
-  nowDepth["origin"] = (string,i)
-  
-  if globalMax <= maxCnt:
-    
-    nowDepth = treeD
-    for j in range(splitDepth):
-      nowDepth = nowDepth[string[j]]
-    candidates = []
-    findPair(candidates, nowDepth, string)
-    
-    if len(candidates) == 0:
-      continue
-    
-    candidates.sort(key=lambda x: x[1])
-    pair = [candidates[0],(string, i)]
-    
-    if globalMax < maxCnt:
-      globalMax = maxCnt
-      globalPair = pair
-    elif pair[0][1] < globalPair[0][1]:
-      globalPair = pair
-    elif pair[0][1] == globalPair[0][1]:
-      if globalPair[1][1] > globalPair[1][1]:
-        globalPair = pair
+for _ in range(M):
+  start, end = list(map(int,rl().split()))
+  indegrees[end] += 1
+  edges[start].append(end)
 
-for s in globalPair:
-  print(s[0])
+for _ in range(N):
+  successValues.append(list(map(int,rl().split())))
+
+for vertex in range(1, N + 1):
+  if indegrees[vertex] == 0:
+    q.append(vertex)
+
+while q:
+  now = q.pop(0)
+  sortedCustomers.append(now)
+  for next in edges[now]:
+    indegrees[next] -= 1
+    minTurn[next] += 1
+    if indegrees[next] == 0:
+      q.append(next)
+
+dp = [[[math.inf for _ in range(Y + 1)] for _ in range(X + 1)] for _ in range(N + 1)]
+dp[0][0][0] = 0
+
+for i in range(1, N + 1):
+  x_i, y_i = successValues[i]
+  for x in range(X + 1):
+    for y in range(Y + 1):
+      dp[i][x][y] = min(dp[i][x][y], dp[i-1][x][y])
+      print(x_i, y_i)
+      if x < x_i or y < y_i:
+        continue
+      dp[i][x][y] = min(dp[i-1][x][y], dp[i-1][x-x_i][y-y_i] + 1)
+
+print(dp)
+
+if len(sortedCustomers) < N:
+  print(-1)
+else:
+  print(sortedCustomers)
+
+for customer in sortedCustomers:
+  print('['+ str(customer)+']' + str(successValues[customer])+', minTurn: ' + str(minTurn[customer]))
