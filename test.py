77c6bf14923f36da0e61f09deca6228c65b5b469
bbbjihan <parkjihan.dev@gmail.com>
2024-07-26 22:56:14 +0900

    240726

--- test.py
+++ test.py
@@ -1,29 +1,6 @@
 import sys;rl=sys.stdin.readline
 
-N = int(rl())
-rects = []
-circles = []
+origins = ['a', 'c', 'i', 'j', 'n', 'o', 't', 'v', 'w']
+editteds = ['@', '[', '!', ';', '^', '0', '7', "\\'", "\\\\'"]
 
-for _ in range(N):
-    inp = rl().split()
-    if inp[0] == 'rectangle':
-        t, x_1, y_1, x_2, y_2 = inp
-        rects.append(((int(x_1), int(y_1)), (int(x_2), int(y_2))))
-    else:
-        t, x, y, r = inp
-        circles.append((int(x), int(y), int(r)))
-
-M = int(rl())
-for _ in range(M):
-    x, y = map(int,rl().split())
-    cnt = 0
-    
-    for (x_1, y_1), (x_2, y_2) in rects:
-        if x_1 <= x <= x_2 and y_1 <= y <= y_2:
-            cnt += 1
-    
-    for x_o, y_o, r in circles:
-        if (x - x_o) ** 2 + (y - y_o) ** 2 <= r ** 2:
-            cnt += 1
-    
-    print(cnt)
+print(editteds[8])
