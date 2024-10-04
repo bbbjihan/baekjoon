b790db282bcc0f70ac8c064cc816d65f09acdf0a
bbbjihan <parkjihan.dev@gmail.com>
2024-10-04 14:05:50 +0900

    241004

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 n=int(input())
-for i in range(n-1):
-    print(('*' * (i + 1)) + (' ' * (2 * (n-i-1))) + ('*' * (i + 1)))
-for i in range(n-1, -1 ,-1):
-    print(('*' * (i + 1)) + (' ' * (2 * (n-i-1))) + ('*' * (i + 1)))
+for i in range(1, n+1):
+    print(' '*(n-i) + '*'*(i))
+for i in range(n-1, -1, -1):
+    print(' '*(n-i) + '*'*(i))
