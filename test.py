671e56242e06de83d376120d168fd3c7f5c43ea9
bbbjihan <74421125+bbbjihan@users.noreply.github.com>
2024-10-23 10:58:36 +0900

    241023

--- test.py
+++ test.py
@@ -1,18 +1,8 @@
-n = int(input())
-for _ in range(n):
-    a,u=input().split()
-    
-    if u == 'kg':
-        b=2.2046
-        u='lb'
-    elif u == 'lb':
-        b=0.4536
-        u='kg'
-    elif u == 'l':
-        b=0.2642
-        u='g'
-    else:
-        b=3.7854
-        u='l'
-    
-    print('{:.4f}'.format(float(a) * b)+ ' ' + u)
+t=int(input())
+for _ in range(t):
+    input()
+    n=int(input())
+    a=0
+    for _ in range(n):
+        a+=int(input())
+    print('YES' if a%n== 0 else 'NO')
