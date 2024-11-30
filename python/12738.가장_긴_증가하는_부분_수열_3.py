87d6d95c7a9edd74eb3bdd7c9a676d3342b88ed4
Jihan Park <74421125+bbbjihan@users.noreply.github.com>
2024-11-30 20:09:37 +0900

    241130

---
+++ python/12738.가장_긴_증가하는_부분_수열_3.py
@@ -0,0 +1,28 @@
+N = int(input())
+A = list(map(int,input().split()))
+
+dp = [1 for _ in range(N)]
+find = [-1000000001]
+for i in range(N):
+    a = A[i]
+    start, end = 0, len(find) - 1
+    while start <= end:
+        mid = (start + end) // 2
+        if a == find[mid]:
+            break
+        elif a < find[mid]:
+            end = mid - 1
+        else:
+            start = mid + 1
+    
+    if find[mid] < a:
+        if mid < len(find) - 1:
+            find[mid + 1] = a
+        else:
+            find.append(a)
+        dp[i] = mid + 1
+    else:
+        find[mid] = a
+        dp[i] = mid
+
+print(max(dp))
