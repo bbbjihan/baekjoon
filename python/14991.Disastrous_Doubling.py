56e92e55a1d42e78085aa81f488f73805365d823
Intellectus-Jihan <74421125+bbbjihan@users.noreply.github.com>
2024-12-12 15:51:30 +0900

    241212

--- python/14991.Disastrous_Doubling.py
+++ python/14991.Disastrous_Doubling.py
@@ -2,10 +2,11 @@ n = int(input())
 bs = list(map(int,input().split()))
 
 a = 1
+flag = False
 for b in bs:
     a = ( a * 2 ) - b
     if a < 0:
-        print('error')
+        flag = True
         break
-else:
-    print(a % 1000000007)
+
+print('error' if flag else a % 1000000007)
